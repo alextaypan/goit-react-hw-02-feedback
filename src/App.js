47a57e36075a04935e1f6aba83b17871cb21bee2
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-02-02 16:01:08 +0200

    add basic files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return null;
+  }
+}
+
+export default App;
